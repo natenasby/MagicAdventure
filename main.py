a3d7a5368c87fb264e70d40dfa1549b3b29c1b9d
Nathan Nasby <nathan@nathans-mbp>
2017-05-22 15:54:04 -0400

    Made some more changes

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ This program will be an interesting game for the Python command line
 
 '''
 
-print("This line will be printed. It will be the beginning of the program")
+
+print("You're trapped inside of a house of cards because the evil dark lord has captured you")
+
+tool = input("what magic tool would you like to use to try and get out of this mess? ")
+
+def toolSelect():
+    if  tool == "heart staff":
+        print("Sorry, you did not get out. Bye bye")
+
+
+
