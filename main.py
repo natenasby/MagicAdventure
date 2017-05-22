b75eb8043618e3c0754a16563b8379f322cd7ee7
Nathan Nasby <nathan@nathans-mbp>
2017-05-22 11:11:25 -0400

    Beginning a new python project

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+
+Made by Nathan Nasby
+
+This program will be an interesting game for the Python command line
+
+'''
+
+print("This line will be printed. It will be the beginning of the program")
